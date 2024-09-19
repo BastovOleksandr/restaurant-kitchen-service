e3db10ddf9ebbf738df964ee01f82a96361088da
Olexandr Bastov <iamlucky1990@gmail.com>
2024-09-19 22:03:14 +0300

    add new file admin_filters.py with 2 filters
    DishByDishTypeAdminFilter filtering dish entries
    by dish type. CookByYearsOfExperienceAdminFilter
    filtering cooks by their years of experience.
    Both filters are for Django admin panel

---
+++ kitchen/admin_filters.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+
+from kitchen.models import DishType
+
+
+class DishByDishTypeAdminFilter(admin.SimpleListFilter):
+    title = _("dish type")
+    parameter_name = "type"
+
+    def lookups(self, request, model_admin):
+        return [(obj.pk, _(obj.name)) for obj in DishType.objects.all()]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(dish_type=self.value())
+        return queryset
+
+
+class CookByYearsOfExperienceAdminFilter(admin.SimpleListFilter):
+    title = _("Experience")
+    parameter_name = "year"
+
+    def lookups(self, request, model_admin):
+        filter_ = {
+            5: "less than 5",
+            10: "5-10",
+            15: "10-15",
+            20: "20+",
+        }
+        return [(years, _(title + " years")) for years, title in filter_.items()]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            year = int(self.value())
+            return queryset.filter(
+                Q(years_of_experience__gte=year - 5)
+                & Q(years_of_experience__lte=year - 1)
+            )
+        return queryset
