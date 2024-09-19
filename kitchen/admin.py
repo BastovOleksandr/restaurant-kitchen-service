7f48d05656cc07d544514c955cbeb5739e8ef815
Olexandr Bastov <iamlucky1990@gmail.com>
2024-09-19 22:07:32 +0300

    register Cook, DishType and Dish models in admin.py
    Added search by 'last_name' for Cook list page,
    by 'name' for DishType list page and by 'name'
    for Dish list page. Added corresponding filters
    for CookAdmin and DishAdmin classes. Add some
    changes to list_display, fieldsets and
    add_fieldsets for better perception

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from kitchen.models import Cook, DishType, Dish
+from kitchen import admin_filters
+
+admin.site.unregister(Group)
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    search_fields = [
+        "last_name",
+    ]
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "years_of_experience",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = [admin_filters.CookByYearsOfExperienceAdminFilter]
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "years_of_experience",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    search_fields = [
+        "name",
+    ]
+    list_display = [
+        "name",
+    ]
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    search_fields = [
+        "name",
+    ]
+    list_display = [
+        "name",
+        "dish_type__name",
+        "description",
+        "price",
+    ]
+    list_filter = [
+        admin_filters.DishByDishTypeAdminFilter,
+    ]
