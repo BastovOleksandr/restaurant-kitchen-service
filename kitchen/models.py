b582d1a055cf2c245ee01a2b037345e955366e4b
Olexandr Bastov <iamlucky1990@gmail.com>
2024-09-27 21:48:37 +0300

    delete unnecessary verbose_name's from some models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,19 +1,16 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AbstractUser
 from django.db.models.functions import Lower
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 
+from restaurant_kitchen_service.settings import AUTH_USER_MODEL
+
 
 class Cook(AbstractUser):
-    first_name = models.CharField(
-        max_length=150, verbose_name="First Name"
-    )
-    last_name = models.CharField(
-        max_length=150, verbose_name="Last Name"
-    )
-    email = models.EmailField(verbose_name="Email Address")
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
     years_of_experience = models.IntegerField(
         default=0,
         verbose_name="years of experience",
@@ -34,7 +31,6 @@ class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name_plural = "dish types"
         ordering = (Lower("name"),)
 
     def __str__(self) -> str:
@@ -46,15 +42,13 @@ class Dish(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
-    cooks = models.ManyToManyField(get_user_model())
+    cooks = models.ManyToManyField(AUTH_USER_MODEL)
 
     class Meta:
         verbose_name_plural = "dishes"
         ordering = (Lower("name"),)
         default_related_name = "dishes"
-        indexes = [
-            models.Index(fields=["price"])
-        ]
+        indexes = [models.Index(fields=["price"])]
 
     def __str__(self) -> str:
         return f'dish: "{self.name}"'
